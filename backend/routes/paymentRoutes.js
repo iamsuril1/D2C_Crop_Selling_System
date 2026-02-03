2b23bda1a771a0a7fbf403652781699fc0d5ea37
Suril Pokharel <surilpokharel3@gmail.com>
2026-02-03 16:08:39 +0545

    Fixed Payment

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { protect, authorize } from "../middleware/authMiddleware.js";
+import { protect } from "../middleware/authMiddleware.js";
+import { authorize } from "../middleware/roleMiddleware.js";
+
 import upload from "../middleware/uploadMiddleware.js";
 import {
   getFarmerPaymentMethods,
